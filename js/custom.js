def6e93699eda4e03f699205c612fb29a40d29c8
Matt Harris <mateodelay@gmail.com>
2015-11-03 11:07:26 -0700

    Added click event to div tag

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,5 @@
  * Created by Matt on 11/3/2015.
  */
 function clickEvent(){
-	document.getElementById("sandwich").style.backgroundColor ="blue";
+	document.getElementById("sandwich").style.color ="blue";
 }

c9a3ed019f603d89ffffba2318713579cacca2d7
Matt Harris <mateodelay@gmail.com>
2015-11-03 10:22:11 -0700

    Added 3 mitch hedberg jokes to the index.php file

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<HTML lang="eng">
+	<!--Header for java-assignment-->
+	<head>
+		<meta charset="UTF-8">
+		<title>Java Assignment</title>
+	</head>
+	<body>
+		<!-- track 2, "sandwiches"-->
+		<p>I order the club sandwich all the time, but I'm not even a member, man. I don't know how I get away with it.
+			How'd it start anyway? I like my sandwiches with three pieces of bread. So do I! Well let's form a club then.
+			Alright, but we need more stipulations. Yes we do; instead of cutting the sandwich once, let's cut it again.
+			Yes, four triangles, and we will position them into a circle. In the middle we will dump chips. Or potato salad.
+			Okay. I got a question for ya, how do you feel about frilly toothpicks? I'm for 'em! Well this club is formed;
+			spread the word on menus nationwide. I like my sandwiches with alfalfa sprouts. Well then you're not in the
+			fuckin' club!
+		</p>
+		<!--track 11, " Three easy payments-->
+		<p>So it said "You can have this product for four easy payments of 19.95." I would like to have a product that was
+			available for three easy payments, and one fuckin' complicated payment! We ain't gonna tell you which payment
+			it is, but one of these payments is gonna be a bitch. The mailman will get shot to death, the envelope will not
+			seal, and the stamp will be in the wrong denomination; good luck, fucker! The last payment must be made in wampum!
+		</p>
+		<!--Doughnuts-->
+		<p>I bought a doughnut and they gave me a receipt for the doughnut. I don't need a receipt for the doughnut.
+			I'll just give you the money, and you give me the doughnut, end of transaction. We don't need to bring ink and
+			paper into this. I just can't imagine a scenario where I would have to prove that I bought a doughnut. Some
+			skeptical friend: "Don't even act like I didn't get that doughnut!
+			I got the documentation right here...oh, wait it's at home...in the file...under "D".
+		</p>
+	</body>
+
+
+
+
+
+
+</HTML>
